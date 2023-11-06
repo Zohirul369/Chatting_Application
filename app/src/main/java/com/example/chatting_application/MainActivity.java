cc0eb8065d4a50d7864e02931322211f85bf81d5
Zohirul <41046676+Zohirul@users.noreply.github.com>
2023-11-06 22:22:54 +0600

    menu add

--- app/src/main/java/com/example/chatting_application/MainActivity.java
+++ app/src/main/java/com/example/chatting_application/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.chatting_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.example.chatting_application.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
