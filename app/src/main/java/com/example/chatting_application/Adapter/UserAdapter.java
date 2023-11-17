214c78a24d67f3a317f83068340463f02fe94d0a
Zohirul <41046676+Zohirul@users.noreply.github.com>
2023-11-17 13:46:57 +0600

    chat page done

--- app/src/main/java/com/example/chatting_application/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatting_application/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.example.chatting_application.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.chatting_application.ChatDetailActivity;
 import com.example.chatting_application.Model.User;
 import com.example.chatting_application.R;
 import com.squareup.picasso.Picasso;
@@ -39,6 +41,17 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         User user =list.get(position);
         Picasso.get().load(user.getProfilePic()).placeholder(R.drawable.avatar).into(holder.image);
         holder.userName.setText(user.getUserName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ChatDetailActivity.class);
+                intent.putExtra("userId", user.getUserId());
+                intent.putExtra("profilePic", user.getProfilePic());
+                intent.putExtra("userName", user.getUserName());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
