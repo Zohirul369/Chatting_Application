f08a9ec92270c799360c2af871fdc14eabe4445f
Zohirul <41046676+Zohirul@users.noreply.github.com>
2023-11-20 01:04:30 +0600

    chat send button work done and create chatRoom on database and create sender and receiver section

--- app/src/main/java/com/example/chatting_application/ChatDetailActivity.java
+++ app/src/main/java/com/example/chatting_application/ChatDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.chatting_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +10,13 @@ import android.view.View;
 import com.example.chatting_application.Adapter.ChatAdapter;
 import com.example.chatting_application.Model.Messages;
 import com.example.chatting_application.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatDetailActivity extends AppCompatActivity {
 
@@ -52,5 +55,40 @@ public class ChatDetailActivity extends AppCompatActivity {
 
         final ArrayList<Messages> messagesModels = new ArrayList<>();
         final ChatAdapter chatAdapter = new ChatAdapter(messagesModels,this,receiverId);
+
+        binding.chatRecyclearView.setAdapter(chatAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclearView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId + receiverId;
+        final String receiverRoom = receiverId + senderId;
+
+        binding.sendmgs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messages = binding.chatTextMgs.getText().toString();
+                final Messages modelMessages = new Messages(senderId,messages);
+                modelMessages.setTimestamp(new Date().getTime());
+                binding.chatTextMgs.setText("");
+
+                database.getReference().child("Chats")
+                        .child(senderRoom)
+                        .push()
+                        .setValue(modelMessages).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                database.getReference().child("Chats")
+                                        .child(receiverRoom)
+                                        .push()
+                                        .setValue(modelMessages).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
     }
 }
